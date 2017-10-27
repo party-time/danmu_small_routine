e4a2302b4b2f5a71565b883bc44f77b39a6728a9
soaryang <longfei19880>
2017-10-27 18:13:07 +0800

    commit

--- pages/danmu/danmu.js
+++ pages/danmu/danmu.js
@@ -1,77 +1,18 @@
-// pages/game/game.js
 Page({
   data: {
-    hidden: false
+    selected: true,
+    selected1: false
   },
-  start: function (event) {
-    /*wx.playBackgroundAudio({
-      dataUrl: 'http://sc1.111ttt.com/2017/1/05/09/298092042172.mp3',
-      success: function (res) {
-
-      }, fail: function (res) {
-
-      }, complete: function (res) {
-
-      }
-    })*/
+  selected: function (e) {
+    this.setData({
+      selected1: false,
+      selected: true
+    })
   },
-
-  pause: function (event) {
-    wx.pauseBackgroundAudio();
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  selected1: function (e) {
+    this.setData({
+      selected: false,
+      selected1: true
+    })
   }
 })
